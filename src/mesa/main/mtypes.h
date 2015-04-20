cb14ca7219e5d5ebc54b690addd8507e21022861
Dave Airlie <airlied@redhat.com>
2015-04-20 10:26:09 +1000

    mesa/mtypes: add gl_subroutine_function and uniform storage to shader
    This adds the necessary storage for subroutine info to gl_shader.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -2371,6 +2371,15 @@ struct gl_ati_fragment_shader_state
    struct ati_fragment_shader *Current;
 };
 
+/**
+ *  Shader subroutine function definition
+ */
+struct gl_subroutine_function
+{
+   char *name;
+   int num_compat_types;
+   const struct glsl_type **types;
+};
 
 /**
  * A GLSL vertex or fragment shader object.
@@ -2521,6 +2530,12 @@ struct gl_shader
        */
       unsigned LocalSize[3];
    } Comp;
+
+   GLuint NumSubroutineUniformTypes;
+   GLuint NumSubroutineUniforms;
+   struct gl_uniform_storage **SubroutineUniformRemapTable;
+   GLuint NumSubroutineFunctions;
+   struct gl_subroutine_function *SubroutineFunctions;
 };
 
 
