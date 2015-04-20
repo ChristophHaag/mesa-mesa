49d2df132f5a81b2a1c7c3c08aa81a08442b7c0e
Dave Airlie <airlied@redhat.com>
2015-04-20 10:28:40 +1000

    program: add subroutine uniform support
    Add support for the subroutine uniform type ir->mesa.cpp
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/mesa/program/ir_to_mesa.cpp
+++ src/mesa/program/ir_to_mesa.cpp
@@ -533,6 +533,7 @@ type_size(const struct glsl_type *type)
       return size;
    case GLSL_TYPE_SAMPLER:
    case GLSL_TYPE_IMAGE:
+   case GLSL_TYPE_SUBROUTINE:
       /* Samplers take up one slot in UNIFORMS[], but they're baked in
        * at link time.
        */
@@ -2439,6 +2440,7 @@ _mesa_associate_uniform_storage(struct gl_context *ctx,
 	    break;
 	 case GLSL_TYPE_SAMPLER:
 	 case GLSL_TYPE_IMAGE:
+         case GLSL_TYPE_SUBROUTINE:
 	    format = uniform_native;
 	    columns = 1;
 	    break;
