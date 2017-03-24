d6b955cf82a9e58c30821e4ab9a776c8043638df
Christoph Haag <haagch+mesadev@frickel.club>
2017-03-24 16:42:23 +0100

    support GALLIUM_HUD=low-fps graph
    record the highest frame time in a period, then display 1/highest_frametime

--- src/gallium/auxiliary/hud/hud_fps.c
+++ src/gallium/auxiliary/hud/hud_fps.c
@@ -36,6 +36,9 @@ struct fps_info {
    boolean frametime;
    int frames;
    uint64_t last_time;
+   uint64_t last_frame;
+   uint64_t largest_time;
+   boolean low_fps;
 };
 
 static void
@@ -43,6 +46,14 @@ query_fps(struct hud_graph *gr, struct pipe_context *pipe)
 {
    struct fps_info *info = gr->query_data;
    uint64_t now = os_time_get();
+
+   if (info->low_fps) {
+      uint64_t timediff = now - info->last_frame;
+      if (timediff > info->largest_time) {
+         info->largest_time = timediff;
+      }
+      info->last_frame = now;
+   }
 
    info->frames++;
 
@@ -53,10 +64,16 @@ query_fps(struct hud_graph *gr, struct pipe_context *pipe)
          info->last_time = now;
       }
       else if (info->last_time + gr->pane->period <= now) {
-         double fps = ((uint64_t)info->frames) * 1000000 /
-                      (double)(now - info->last_time);
+         double fps;
+         if (info->low_fps) {
+            fps = 1. / (info->largest_time / 1000000.);
+         } else {
+            fps = ((uint64_t)info->frames) * 1000000 /
+                        (double)(now - info->last_time);
+         }
          info->frames = 0;
          info->last_time = now;
+         info->largest_time = 1; //whatever
 
          hud_graph_add_value(gr, fps);
       }
@@ -89,11 +106,43 @@ hud_fps_graph_install(struct hud_pane *pane)
    struct fps_info *info = gr->query_data;
    info->frametime = false;
 
+   struct fps_info *info = gr->query_data;
+   info->low_fps = false;
+
    gr->query_new_value = query_fps;
 
    /* Don't use free() as our callback as that messes up Gallium's
     * memory debugger.  Use simple free_query_data() wrapper.
     */
+   gr->free_query_data = free_query_data;
+
+   hud_pane_add_graph(pane, gr);
+}
+
+void
+hud_low_fps_graph_install(struct hud_pane *pane)
+{
+    struct hud_graph *gr = CALLOC_STRUCT(hud_graph);
+
+    if (!gr)
+	return;
+
+    strcpy(gr->name, "low-fps");
+    gr->query_data = CALLOC_STRUCT(fps_info);
+    if (!gr->query_data) {
+	FREE(gr);
+	return;
+    }
+
+    struct fps_info *info = gr->query_data;
+    info->low_fps = true;
+    info->last_frame = os_time_get();
+
+    gr->query_new_value = query_fps;
+
+    /* Don't use free() as our callback as that messes up Gallium's
+     * memory debugger.  Use simple free_query_data() wrapper.
+     */
    gr->free_query_data = free_query_data;
 
    hud_pane_add_graph(pane, gr);
@@ -118,7 +167,7 @@ hud_frametime_graph_install(struct hud_pane *pane)
 
    gr->query_new_value = query_fps;
 
-   gr->free_query_data = free_query_data;
+    gr->free_query_data = free_query_data;
 
-   hud_pane_add_graph(pane, gr);
+    hud_pane_add_graph(pane, gr);
 }
