38fc1f2faba5fccc8679d43b0da2770c54b5a510
Keith Packard <keithp@keithp.com>
2018-02-09 17:27:38 -0800

    radv: Add VK_GOOGLE_display_timing extension to radv driver
    This adds support for the VK_GOOGLE_display timing extension.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/amd/vulkan/radv_extensions.py
+++ src/amd/vulkan/radv_extensions.py
@@ -100,6 +100,7 @@ EXTENSIONS = [
     Extension('VK_AMD_rasterization_order',               1, 'device->rad_info.chip_class >= VI && device->rad_info.max_se >= 2'),
     Extension('VK_AMD_shader_info',                       1, True),
     Extension('VK_MESA_query_timestamp',                  1, True),
+    Extension('VK_GOOGLE_display_timing',                 1, True),
 ]
 
 class VkVersion:
