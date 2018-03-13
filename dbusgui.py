678d150824d5c665becd05e600d7bff2c26b8cee
Christoph Haag <haagch@frickel.club>
2018-03-13 13:42:36 +0100

    protype gui

--- dbusgui.py
+++ dbusgui.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sys
+
+from PyQt5.QtWidgets import (QWidget, QPushButton, QListWidget, QListWidgetItem, QHBoxLayout, QVBoxLayout, QApplication)
+from PyQt5.QtCore import pyqtSlot
+
 from pydbus import SessionBus
 
 bus = SessionBus()
@@ -10,10 +14,129 @@ o = bus.get('mesa.hud')
 
 #print("Application:", o.ApplicationBinary)
 
-config = "fps" if len(sys.argv) < 2 else sys.argv[1]
-print("Setting graph config to:", config)
-o.GraphConfiguration(config)
+#config = "fps" if len(sys.argv) < 2 else sys.argv[1]
+#print("Setting graph config to:", config)
+#o.GraphConfiguration(config)
 
 #reply = o.Configure(0)
 #print(reply)
 
+
+class PidList (QListWidget):
+        def Clicked(self, item):
+                print(item)
+
+
+class ConfigList (QListWidget):
+        def Clicked(self, item):
+                print(item)
+
+
+class AvailableConfigList (QListWidget):
+        def Clicked(self, item):
+                print(item.text())
+
+
+class Example(QWidget):
+    pidlist = None
+    configlist = None
+    available = None
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    @pyqtSlot()
+    def add_click(self):
+        assert isinstance(self.available, QListWidget)
+        assert isinstance(self.configlist, QListWidget)
+        if len(self.available.selectedItems()) == 0: return
+        item = self.available.selectedItems()[0]
+        #print(item.text())
+        self.configlist.addItem(item.text())
+
+    @pyqtSlot()
+    def remove_click(self):
+        assert isinstance(self.configlist, QListWidget)
+        model = self.configlist.model()
+        for selectedItem in self.configlist.selectedItems():
+            qIndex = self.configlist.indexFromItem(selectedItem)
+            model.removeRow(qIndex.row())
+
+    @pyqtSlot()
+    def set_click(self):
+        config = ""
+        assert isinstance(self.configlist, QListWidget)
+        for i in range(self.configlist.count()):
+            config += self.configlist.item(i).text()
+            if i < self.configlist.count() - 1:
+                config += ","
+        print("Setting config to \"" + config + "\"")
+        o.GraphConfiguration(config)
+
+    def initUI(self):
+
+        pidlist = PidList()
+        configlist = ConfigList()
+        available = AvailableConfigList()
+        self.pidlist = pidlist
+        self.configlist = configlist
+        self.available = available
+
+        okButton = QPushButton("Set")
+        cancelButton = QPushButton("Cancel")
+        okButton.clicked.connect(self.set_click)
+
+        availableButtons = QHBoxLayout()
+        availableAddButton = QPushButton("Add")
+        availableButtons.addWidget(availableAddButton)
+        availableAddButton.clicked.connect(self.add_click)
+
+        availableLayout = QVBoxLayout()
+        availableLayout.addWidget(available)
+        availableLayout.addLayout(availableButtons)
+
+        configLayout = QVBoxLayout()
+        configLayout.addWidget(configlist)
+        configRemoveButton = QPushButton("Remove")
+        configLayout.addWidget(configRemoveButton)
+        configRemoveButton.clicked.connect(self.remove_click)
+
+
+        # pidlist.itemClicked.connect(pidlist.Clicked)
+        # pidlist.itemClicked.connect(pidlist.Clicked)
+        # available.itemClicked.connect(available.Clicked)
+
+        available.addItem(QListWidgetItem("fps"))
+        available.addItem(QListWidgetItem("cpu"))
+
+        allvertical = QVBoxLayout()
+
+        mainguihoriz = QHBoxLayout()
+
+        mainguihoriz.addWidget(pidlist)
+        mainguihoriz.addLayout(configLayout)
+        mainguihoriz.addLayout(availableLayout)
+        mainguihoriz.addStretch(1)
+
+        mainbuttons = QHBoxLayout()
+        mainbuttons.addStretch(1)
+        mainbuttons.addWidget(okButton)
+        mainbuttons.addWidget(cancelButton)
+
+
+        allvertical.addLayout(mainguihoriz)
+        allvertical.addLayout(mainbuttons)
+
+        self.setLayout(allvertical)
+
+        # self.setGeometry(300, 300, 300, 150)
+        self.setGeometry(0,0,800,600)
+        self.setWindowTitle('Mesa HUD GUI prototype')
+        self.show()
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
